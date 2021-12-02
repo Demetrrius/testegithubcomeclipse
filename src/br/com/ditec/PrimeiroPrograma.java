f88924e4c50b4fb622faf8dea0a953921d4deee7
Demetrrius <demein@gmail.com>
2021-12-02 08:35:18 -0300

    Class Livros

--- src/br/com/ditec/PrimeiroPrograma.java
+++ src/br/com/ditec/PrimeiroPrograma.java
@@ -12,11 +12,17 @@ public class PrimeiroPrograma {
 		cachorro.setRaca("S.R.D");
 		cachorro.setIdade(10);
 		
+		Livros livros = new Livros();
+		
 		System.out.println("Nome " + cachorro.getNome());
 		System.out.println("Cor  " + cachorro.getCor());
 		System.out.println("Raca " + cachorro.getRaca());
 		System.out.println("Idde " + cachorro.getIdade());
 		
-		
+		System.out.println(livros);
 	}
+}
+class Livros{
+	private String nome;
+	private String npag;
 }
